c5ab6e97b286dc81de8cb5af10674c0e5b15ef58
ar razy fathan rabbani <arrazy.rabbani266@gmail.com>
2022-03-14 12:44:47 +0700

    add: Navigation Bottom

--- app/src/main/java/com/example/kbbikamusbesarbahasaindonesia/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/kbbikamusbesarbahasaindonesia/ui/home/MainActivity.kt
@@ -9,73 +9,36 @@ import android.view.View
 import android.widget.Toast
 import androidx.annotation.MenuRes
 import androidx.appcompat.widget.PopupMenu
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.kbbikamusbesarbahasaindonesia.R
+import com.example.kbbikamusbesarbahasaindonesia.databinding.ActivityMainBinding
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var button: MaterialButton
-    private lateinit var card: MaterialCardView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        button = findViewById(R.id.button)
-        card = findViewById(R.id.card1)
+        setupBottomNavigationView()
 
-        card.setOnClickListener {
-            showMenu(it, R.menu.sample_menu)
-        }
+    }
 
-
-        button.setOnClickListener {
-            MaterialAlertDialogBuilder(this)
-                .setTitle("This is dialog")
-                .setMessage("Lorerm itsum dolro sit ama afas asfas")
-                .setNeutralButton("Cancel") { dialog, which ->
-                    dialog.cancel()
-                }
-                .setPositiveButton("Yes") { dialog, which ->
-                    dialog.cancel()
-                }
-                .setNegativeButton("Decline") { dialog, which ->
-                    dialog.cancel()
-                }
-                .show()
+    private fun setupBottomNavigationView() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.apply {
+            setupWithNavController(navController)
+            itemIconTintList = null
         }
 
     }
 
-    private fun showMenu(v: View, @MenuRes menuRes: Int) {
-        val popup = PopupMenu(this, v)
-        popup.menuInflater.inflate(menuRes, popup.menu)
-        popup.show()
-    }
-
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.sample_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.option_1 -> {
-                Toast.makeText(this, "option 1 pressed", Toast.LENGTH_LONG).show()
-            }
-            R.id.option_2 -> {
-                Toast.makeText(this, "option 2 pressed", Toast.LENGTH_LONG).show()
-
-            }
-            R.id.option_3 -> {
-                Toast.makeText(this, "option 3 pressed", Toast.LENGTH_LONG).show()
-
-            }
-        }
-        return true
-    }
 }
