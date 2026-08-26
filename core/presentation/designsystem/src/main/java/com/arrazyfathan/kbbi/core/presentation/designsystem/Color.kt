e014cfc6f8cc11a139815a096302194d6fb279e1
arrazyfathan <arrazy.rabbani266@gmail.com>
2026-08-26 11:18:10 +0700

    feat: add new theme color palettes to design system
    Introduce primary and secondary color constants for various themes to support extended UI customization.
    
    Changes:
    - **Design System**:
        - Add `RoyalOcean` primary and secondary colors.
        - Add `GoldenSunset` primary and secondary colors.
        - Add `GoldenCoralEnergy` primary and secondary colors.
        - Add `DeepForestEnergy` primary and secondary colors.

--- core/presentation/designsystem/src/main/java/com/arrazyfathan/kbbi/core/presentation/designsystem/Color.kt
+++ core/presentation/designsystem/src/main/java/com/arrazyfathan/kbbi/core/presentation/designsystem/Color.kt
@@ -11,3 +11,15 @@ val TextP = Color(0xFF7C8796)
 val Red = Color(0xFFF3254F)
 val MoreRed = Color(0xFFA60B2B)
 val Grey = Color(0xFFD6D6D6)
+
+val RoyalOceanPrimary = Color(0xFF303E9F)
+val RoyalOceanSecondary = Color(0xFF5763B1)
+
+val GoldenSunsetPrimary = Color(0xFFD5972A)
+val GoldenSunsetSecondary = Color(0xFFE5C173)
+
+val GoldenCoralEnergyPrimary = Color(0xFFC93939)
+val GoldenCoralEnergySecondary = Color(0xFFEF6464)
+
+val DeepForestEnergyPrimary = Color(0xFF2E494C)
+val DeepForestEnergySecondary = Color(0xFF507E83)
